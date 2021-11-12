45b1098dc962be3869ff942723b2a4cf984ff8ad
mehdi <0r0.developer@gmail.com>
2021-11-13 00:51:35 +0400

    change test code for binary search

--- src/test/java/com/example/myproject/BinarySearchImplTest.java
+++ src/test/java/com/example/myproject/BinarySearchImplTest.java
@@ -1,5 +1,15 @@
 package com.example.myproject;
 
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
 public class BinarySearchImplTest {
+
+    public BinarySearchImpl binarySearchImpl(){
+        return Mockito.mock(BinarySearchImpl.class);
+    }
 
 }
