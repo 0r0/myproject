a77eac27db573ff563c9d3d97e32e1cbe6ff990d
mehdi <0r0.developer@gmail.com>
2021-11-14 21:48:16 +0400

    remove binarysearch method and add test version of it

--- src/main/java/com/example/myproject/BinarySearchImpl.java
+++ src/main/java/com/example/myproject/BinarySearchImpl.java
@@ -1,23 +1,30 @@
 package com.example.myproject;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BinarySearchImpl {
     //sort
     //search
-    public int binarySearch(int[] numbers, int left, int right, int numberToSearchFor) {
-        if (right >= left) {
-            int middle = ((right - 1) + left) / 2;
-            if (middle == numberToSearchFor)
-                return middle;
-            if (numbers[middle] > numberToSearchFor)
-                return binarySearch(numbers, left, middle - 1, numberToSearchFor);
+//    public int binarySearch(int[] numbers, int left, int right, int numberToSearchFor) {
+//        if (right >= left) {
+//            int middle = ((right - 1) + left) / 2;
+//            if (middle == numberToSearchFor)
+//                return middle;
+//            if (numbers[middle] > numberToSearchFor)
+//                return binarySearch(numbers, left, middle - 1, numberToSearchFor);
+//
+//            return binarySearch(numbers, middle + 1, right, numberToSearchFor);
+//        }
+//        return -1;
+//
+//    }
+//    private int[] sortedArray(int[] numbers) {
+//        return null;
+//
+//    }
 
-            return binarySearch(numbers, middle + 1, right, numberToSearchFor);
-        }
-        return -1;
-
-    }
-    private int[] sortedArray(int[] numbers) {
-        return null;
-
+    public int binarySearch(int[] numbers,int numberToSearchFor){
+        return 3;
     }
 }
