cd5ed1cbed81c2d1bead2a4000041969be3ba8eb
mehdi <0r0.developer@gmail.com>
2021-11-16 00:45:45 +0400

    get the bean and run binary search method and print result

--- src/main/java/com/example/myproject/MyprojectApplication.java
+++ src/main/java/com/example/myproject/MyprojectApplication.java
@@ -2,15 +2,19 @@ package com.example.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyprojectApplication {
 
     public static void main(String[] args) {
-        BinarySearchImpl binarySearch=new BinarySearchImpl(new BubbleSortAlgorithm());
+//        BinarySearchImpl binarySearch=new BinarySearchImpl(new BubbleSortAlgorithm());
+//
+//        System.out.println(result);
+        ApplicationContext applicationContext = SpringApplication.run(MyprojectApplication.class, args);
+        BinarySearchImpl binarySearch=applicationContext.getBean(BinarySearchImpl.class);
         int result=binarySearch.binarySearch(new int[]{12,4,6},3);
         System.out.println(result);
-        SpringApplication.run(MyprojectApplication.class, args);
 
     }
 
