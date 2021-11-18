b9e982a9bd5c547719c25a3adab531665056d3d1
mehdi <0r0.developer@gmail.com>
2021-11-18 20:35:24 +0400

    add qualifier and scope annotation for make it as prototype

--- src/main/java/com/example/myproject/BubbleSortAlgorithm.java
+++ src/main/java/com/example/myproject/BubbleSortAlgorithm.java
@@ -1,10 +1,16 @@
 package com.example.myproject;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("bubble")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BubbleSortAlgorithm implements SortAlgorithm{
     public int[] sort(int[] numbers) {
         return  numbers;
     }
+
 }
