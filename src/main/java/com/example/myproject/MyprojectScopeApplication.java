7894de981281bcbbde16d41df7085c419bea7486
mehdi <0r0.developer@gmail.com>
2021-11-18 21:10:38 +0400

    make  an main class for scope package

---
+++ src/main/java/com/example/myproject/MyprojectScopeApplication.java
@@ -0,0 +1,17 @@
+package com.example.myproject;
+
+import com.example.myproject.basic.BinarySearchImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MyprojectScopeApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = SpringApplication.run(MyprojectScopeApplication.class, args);
+
+    }
+
+}
