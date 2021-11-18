94f96e851902aa598db36dd1d14edf138315e3d0
mehdi <0r0.developer@gmail.com>
2021-11-18 21:30:39 +0400

    create 2 objects of persondoa class and log object and getjdbcConnection method on screen

--- src/main/java/com/example/myproject/MyprojectScopeApplication.java
+++ src/main/java/com/example/myproject/MyprojectScopeApplication.java
@@ -1,17 +1,25 @@
 package com.example.myproject;
 
-import com.example.myproject.basic.BinarySearchImpl;
+
+import com.example.myproject.scope.PersonDoa;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyprojectScopeApplication {
-
+    private static Logger LOGGER= LoggerFactory.getLogger(MyprojectScopeApplication.class);
     public static void main(String[] args) {
-
         ApplicationContext applicationContext = SpringApplication.run(MyprojectScopeApplication.class, args);
+        PersonDoa personDoa =applicationContext.getBean(PersonDoa.class);
+        PersonDoa personDoa2=applicationContext.getBean(PersonDoa.class);
 
+        LOGGER.info("{}",personDoa);
+        LOGGER.info("{}",personDoa.getJdbcConnection());
+        LOGGER.info("{}",personDoa2);
+        LOGGER.info("{}",personDoa2.getJdbcConnection());
     }
 
 }
