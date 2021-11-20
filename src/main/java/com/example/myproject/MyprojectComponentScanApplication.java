a2a40e49ab3cf5612175fa330aa235895baa231a
mehdi <0r0.developer@gmail.com>
2021-11-20 19:08:46 +0400

    apllication class for Component package

---
+++ src/main/java/com/example/myproject/MyprojectComponentScanApplication.java
@@ -0,0 +1,30 @@
+package com.example.myproject;
+
+import com.example.componentscan.ComponentDoa;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example.componentscan")
+public class MyprojectComponentScanApplication {
+    private static Logger LOGGER= LoggerFactory.getLogger(MyprojectScopeApplication.class);
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = SpringApplication.run(MyprojectComponentScanApplication.class, args);
+        ComponentDoa componentDoa1=applicationContext.getBean(ComponentDoa.class);
+        ComponentDoa componentDoa2=applicationContext.getBean(ComponentDoa.class);
+        LOGGER.info("{}",componentDoa1);
+        LOGGER.info("{}",componentDoa1.getComponentJdbcConnection());
+        LOGGER.info("{}",componentDoa2);
+        LOGGER.info("{}",componentDoa2.getComponentJdbcConnection());
+
+
+
+    }
+
+}
