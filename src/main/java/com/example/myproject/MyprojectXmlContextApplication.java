7245bc42d888052ba39769518c8a84106967d99a
mehdi <0r0.developer@gmail.com>
2021-11-20 23:35:22 +0400

    remove redundant code

--- src/main/java/com/example/myproject/MyprojectXmlContextApplication.java
+++ src/main/java/com/example/myproject/MyprojectXmlContextApplication.java
@@ -1,6 +1,6 @@
 package com.example.myproject;
 
-import com.example.myproject.basic.BinarySearchImpl;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,19 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class MyprojectXmlContextApplication {
 
     public static void main(String[] args) {
-//        BinarySearchImpl binarySearch=new BinarySearchImpl(new BubbleSortAlgorithm());
-//
-//        System.out.println(result);
+
         try(AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyprojectXmlContextApplication.class)) {
-//                SpringApplication.run(MyprojectBasicApplication.class, args);
-            BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
-            BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
-            BinarySearchImpl binarySearch3 = applicationContext.getBean(BinarySearchImpl.class);
-            System.out.println(binarySearch1);
-            System.out.println(binarySearch2);
-            System.out.println(binarySearch3);
-            int result = binarySearch1.binarySearch(new int[]{12, 4, 6}, 3);
-            System.out.println(result);
+//
         }
 
     }
